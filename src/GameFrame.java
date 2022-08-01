81b7b8002ca9dbb796ee214a6f747664fb83ab5a
ozumCanBulundu <ozumcanbulundu@gmail.com>
2022-08-01 16:00:00 +0300

    Initial commit

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,13 +1,19 @@
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
+
+    private final int  width = 30;
+    private final int height = 30;
+    private final int dimension = 20;
+    public
+
     GameFrame(){
         this.add(new GamePanel());
         this.setTitle("Snake Game");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
         this.setResizable(false);
-        this.pack();
+        this.setSize(width * dimension, height * dimension);
         this.setLocationRelativeTo(null);
     }
 }
