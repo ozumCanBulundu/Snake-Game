ddfbfa35a9810d9706e8bed743b1e86a846fee78
ozumCanBulundu <ozumcanbulundu@gmail.com>
2022-08-08 11:22:29 +0300

    Update

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,20 +2,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel extends JPanel implements ActionListener {
-
+public class GamePanel extends JPanel implements KeyListener,ActionListener{
 
     private Food food;
     private Snake snake;
+
     public Graphics g;
 
-    GamePanel(){
+    private Timer t = new Timer(100,this);
 
+    GamePanel(){
+        t.start();
     }
 
-    public void startGame(){
+    @Override
+    public void keyTyped(KeyEvent e) {}
 
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keycode = e.getKeyCode();
+
+        if (keycode == KeyEvent.VK_W || keycode == KeyEvent.VK_UP){
+
+        }
+        else if (keycode == KeyEvent.VK_S || keycode == KeyEvent.VK_DOWN){
+
+        }
+        else if (keycode == KeyEvent.VK_D || keycode == KeyEvent.VK_RIGHT){
+
+        }
+        else if (keycode == KeyEvent.VK_A || keycode == KeyEvent.VK_LEFT){
+
+        }
     }
 
     public void paintComponent(Graphics g){
@@ -23,22 +47,6 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g){
-
-    }
-
-    public void move(){
-
-    }
-
-    public void checkPoint(){
-
-    }
-
-    public void checkCollision(){
-
-    }
-
-    public void gameOver(Graphics g) {
 
     }
 
